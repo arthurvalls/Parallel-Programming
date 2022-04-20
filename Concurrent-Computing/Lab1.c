b4d1ada1b65987c5a471bdfbb78e2e00662a1f99
arthurvalls <arthur.valls5@gmail.com>
2022-04-19 22:35:14 -0300

    Lab 1

---
+++ Concurrent-Computing/Lab1.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#define NTHREADS 2
+#define LIMIT 10000
+
+int vetor[LIMIT];
+
+// função que cada thread utilizará para elevar os elementos do vetor de sua respectiva parte ao quadrado
+void *elevar(void *arg)
+{
+    int ident = *(int *)arg;
+    int i = (LIMIT / (ident)) - 1;
+    int j = 0;
+    while (j != (LIMIT / 2))
+    {
+        vetor[i] = (i * i);
+        j++;
+        i--;
+    }
+    pthread_exit(NULL);
+}
+
+int main()
+{
+    int elemento, i;
+
+    for (elemento = 0; elemento < LIMIT; elemento++) // preenche vetor
+    {
+        vetor[elemento] = elemento;
+    }
+
+    pthread_t tid_sistema[NTHREADS]; // identificadores das threads no sistema
+    int ident[NTHREADS];
+
+    for (i = 1; i <= NTHREADS; i++) // cria as threads
+    {
+        ident[i - 1] = i;
+        if (pthread_create(&tid_sistema[i - 1], NULL, elevar, (void *)&ident[i - 1]))
+            printf("--ERRO: pthread_create()\n");
+    }
+
+    // aguarda o término de execução das threads
+    for (i = 0; i < NTHREADS; i++)
+    {
+        if (pthread_join(tid_sistema[i], NULL))
+            printf("--ERRO: pthread_join\n");
+    }
+
+    // testa caso ocorra erros
+    for (i = 0; i < LIMIT; i++)
+    {
+        if (vetor[i] != (i * i))
+        {
+            printf("--ERRO: elevar\n");
+            break;
+        }
+    }
+    return 0;
+}
---
+++ Concurrent-Computing/Respostas Lab 1.txt
@@ -0,0 +1,16 @@
+Respostas Laboratório 1: 
+
+Questão 1) Sim, há mudanças na ordem de execução das threads, 
+visto que a ordem de execução depende da maneira de como o sistema operacional escalonará as threads.
+
+Questão 2) Percebe-se que a ordem de execução permanece a mesma,
+porém através do argumento "ident" utilizado para distinguir uma thread da outra,
+conseguimos visualizar a ordem de execução dos "hello world" de cada thread.
+
+Questão 3) Sim, o programa funcionou, alocando espaço na memória e preenchendo os argumentos,
+os convertendo de ponteiro pra void e no struct fazendo a conversão de volta para
+o tipo que foi definido para a estrutura.
+
+Questão 4) Nas questões anteriores percebemos que em várias das execuções, a thread principal não foi a última a terminar. 
+O que foi consertado nesta questão ao utilizarmos a função join da biblioteca pthread,
+que faz com que a thread principal aguarde as outras terminarem.
