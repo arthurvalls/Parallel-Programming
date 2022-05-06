87aada83b7dc48b02884e014289d777ad5399de5
Arthur Valls <73508310+arthurvalls@users.noreply.github.com>
2022-05-05 22:22:57 -0300

    Update lab3.c

--- Lab 3/lab3.c
+++ Lab 3/lab3.c
@@ -1,64 +1,87 @@
+//Laboratório 3 - Computação Concorrente
+//Nome: Arthur Valls da Costa Silva
+//DRE: 120177470
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include "timer.h"
 
-long int N;  // numero de elementos do vetor
-int nthreads;  // numero de threads
-double *vetor; // vetor de entrada com dimensao N
-double concurrentTime, sequentialTime, largestSeq = 0, smallestSeq = 0, largestConc = 0, smallestConc = 0;
+long long int N; // numero de elementos do vetor
+int nthreads;    // numero de threads
+float *vetor;    // vetor de entrada com dimensao N
+float largestSeq, smallestSeq, largestConc, smallestConc;
 
 // fluxo das threads
 void *tarefa(void *arg)
 {
-    long int id = (long int)arg;         // identificador da thread
+
+    long int id = (long int)arg;      // identificador da thread
     long int tamBloco = N / nthreads; // tamanho do bloco de cada thread
-    long int start = id * tamBloco;     // elemento inicial do bloco da thread
-    long int end;                        // elemento final(nao processado) do bloco da thread
+    long int start = id * tamBloco;   // elemento inicial do bloco da thread
+    long int end;                     // elemento final(nao processado) do bloco da thread
     if (id == nthreads - 1)
         end = N;
     else
         end = start + tamBloco; // trata o resto se houver
+
+    largestConc = vetor[0];
+    smallestConc = vetor[0];
+
     for (long int i = start; i < end; i++)
     {
+
         if (vetor[i] > largestConc)
             largestConc = vetor[i];
         if (vetor[i] < smallestConc)
             smallestConc = vetor[i];
     }
-    pthread_exit(NULL);
+    pthread_exit((NULL));
+}
+
+// preenche o vetor com números aleatórios tipo float até N
+void fillVector(long long int N)
+{
+
+    srand((float)time(0));
+    for (long int i = 0; i < N; i++)
+    {
+        vetor[i] = (float)rand() / RAND_MAX * (float)(N);
+    }
 }
 
 // fluxo principal
 int main(int argc, char *argv[])
 {
-    double start, end; // tomada de tempo
-    pthread_t *tid;    // identificadores das threads no sistema
-    double *retorno;   // valor de retorno das threads
+    double start, end, concurrentTime, sequentialTime;
+    pthread_t *tid;  // identificadores das threads no sistema
+    double *retorno; // valor de retorno das threads
 
+  
     // recebe e valida os parametros de entrada (dimensao do vetor, numero de threads)
     if (argc < 3)
     {
         fprintf(stderr, "Digite: %s <dimensao do vetor> <numero threads>\n", argv[0]);
         return 1;
     }
-    N = atoi(argv[1]);
+    N = atoll(argv[1]);
     nthreads = atoi(argv[2]);
+    
     // aloca o vetor de entrada
-    vetor = (double *)malloc(sizeof(double) * N);
+    vetor = (float *)malloc(sizeof(float) * N);
     if (vetor == NULL)
     {
         fprintf(stderr, "ERRO--malloc\n");
         return 2;
     }
-    // preenche o vetor de entrada
-    srand((float)time(NULL));
-    for (long int i = 0; i < N; i++)
-        vetor[i] =  (float)rand();
+
+    fillVector(N);
 
     // procura sequencial
     GET_TIME(start);
-    for (long int i = 1; i < N; ++i)
+    largestSeq = vetor[0];
+    smallestSeq = vetor[0];
+    for (long int i = 0; i < N; ++i)
     {
         if (vetor[i] > largestSeq)
             largestSeq = vetor[i];
@@ -77,6 +100,7 @@ int main(int argc, char *argv[])
         fprintf(stderr, "ERRO--malloc\n");
         return 2;
     }
+    
     // criar as threads
     for (long int i = 0; i < nthreads; i++)
     {
@@ -86,6 +110,7 @@ int main(int argc, char *argv[])
             return 3;
         }
     }
+    
     // aguardar o termino das threads
     for (long int i = 0; i < nthreads; i++)
     {
@@ -94,18 +119,18 @@ int main(int argc, char *argv[])
             fprintf(stderr, "ERRO--pthread_create\n");
             return 3;
         }
-        
     }
     GET_TIME(end);
     concurrentTime = end - start;
     printf("Tempo concorrente:  %lf\n", concurrentTime);
 
-    printf("Aceleração do programa:  %lf\n", (sequentialTime/concurrentTime));
+    printf("Aceleração do programa:  %lf\n", (sequentialTime / concurrentTime));
 
-    //corretude
-   if(largestSeq != largestConc || smallestSeq != smallestConc){
-       fprintf(stderr, "ERRO--número enconcontrado sequencial e concorrente diferentes");
-   }
+    // corretude
+    if (largestSeq != largestConc || smallestSeq != smallestConc)
+    {
+        fprintf(stderr, "ERRO--número enconcontrado sequencial e concorrente diferentes\n");
+    }
 
     // libera as areas de memoria alocadas
     free(vetor);
