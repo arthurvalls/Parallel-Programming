9bec59a4341b3dfd93eebe5679cea9038cf9273c
Arthur Valls <73508310+arthurvalls@users.noreply.github.com>
2022-05-12 03:18:44 -0300

    hopefully last update

--- Lab 4/lab4.c
+++ Lab 4/lab4.c
@@ -5,38 +5,36 @@
 #include "timer.h"
 
 long long int globalVar = 0, N;
-int nthreads, *vetor;
+int nthreads, *array;
 float *sequentialArray, *concurrentArray;
+pthread_mutex_t mutex;
 
-pthread_mutex_t mutex; // variavel de mutex para exclusao mutua
-
-int isPrime(int number)
+// funcao para encontrar a raiz quadrada de numeros primos
+int isPrime(long long int number)
 {
-
     if (number < 2)
         return 0;
 
     double squareRoot = sqrt(number);
-    for (int i = 2; i <= (squareRoot) + 1; i++)
+    for (int i = 2; i <= (squareRoot); i++)
         if (number % i == 0)
             return 0;
     return 1;
 }
 
-void sequentialPrimes(int *vetor, long long int N)
+void sequentialPrime(int *array, long long int N)
 {
     for (long long int i = 0; i < N; i++)
     {
-        if (isPrime(vetor[i]))
-            sequentialArray[i] = sqrt(vetor[i]);
+        if (isPrime(array[i]))
+            sequentialArray[i] = sqrt(array[i]);
         else
-            sequentialArray[i] = vetor[i];
-    }
+            sequentialArray[i] = array[i];
+    };
 }
 // funcao executada pelas threads
 void *task()
 {
-
     pthread_mutex_lock(&mutex);
     int localVar = globalVar;
     globalVar++;
@@ -44,10 +42,11 @@ void *task()
 
     while (localVar < N)
     {
-        if (isPrime(concurrentArray[localVar]))
-            concurrentArray[localVar] = sqrt(vetor[localVar]);
+        if (isPrime(array[localVar]))
+            concurrentArray[localVar] = sqrt(array[localVar]);
         else
-            concurrentArray[localVar] = vetor[localVar];
+            concurrentArray[localVar] = array[localVar];
+
         pthread_mutex_lock(&mutex);
         localVar = globalVar;
         globalVar++;
@@ -57,74 +56,84 @@ void *task()
     pthread_exit(NULL);
 }
 
+// funcao para verificar a corretude
+void correctness(float *array1, float *array2)
+{
+
+    for (int i = 0; i < N; i++)
+    {
+        if (array1[i] != array2[i])
+        {
+            printf("ERRO: Vetores diferentes.\n");
+            break;
+        }
+        else
+            printf("Vetores iguais.\n");
+        break;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     pthread_t *tid;
-    int t;
+    long int t;
     double start, end;
     double concurrentTime, sequentialTime;
 
     if (argc < 3)
     {
         fprintf(stderr, "Digite: %s <dimensao do vetor> <numero threads>\n", argv[0]);
-        return 1;
+        return 3;
     }
-    N = atoi(argv[1]);
+    N = atoll(argv[1]);
     nthreads = atoi(argv[2]);
 
-    // aloca o vetor de entrada
-    vetor = (int *)malloc(sizeof(int) * N);
-    if (vetor == NULL)
+    // aloca o array de entrada
+    array = (int *)malloc(sizeof(int) * N);
+    if (array == NULL)
     {
-        fprintf(stderr, "ERRO--malloc\n");
+        fprintf(stderr, "ERRO--malloc--Array1\n");
         return 2;
     }
+
     sequentialArray = (float *)malloc(sizeof(float) * N);
-    if (vetor == NULL)
-    {
-        fprintf(stderr, "ERRO--malloc\n");
-        return 2;
-    }
     concurrentArray = (float *)malloc(sizeof(float) * N);
-    if (vetor == NULL)
-    {
-        fprintf(stderr, "ERRO--malloc\n");
-        return 2;
-    }
 
-    // preenche o vetor de entrada
+    // preenche o array de entrada com números aleatórios até 10^6
+
     srand(time(NULL));
-    // int limit = pow(10,6);
-
+    int limit = pow(10, 6);
     for (long int i = 0; i < N; i++)
     {
-        vetor[i] = rand() % N;
-    }
+        array[i] = rand() % limit;
+    };
 
-    // sequencial
+    // função sequencial
     GET_TIME(start);
-    sequentialPrimes(vetor, N);
+    sequentialPrime(array, N);
     GET_TIME(end);
     sequentialTime = end - start;
     printf("Tempo sequencial = %lf\n", sequentialTime);
 
+    // função concorrente
     GET_TIME(start);
     pthread_mutex_init(&mutex, NULL);
+
     tid = (pthread_t *)malloc(sizeof(pthread_t) * nthreads);
     if (tid == NULL)
     {
         fprintf(stderr, "ERRO--malloc\n");
         return 2;
     }
+
     for (t = 0; t < nthreads; t++)
     {
-
         if (pthread_create(tid + t, NULL, task, NULL))
         {
             printf("--ERRO: pthread_create()\n");
             exit(-1);
-        }
-    }
+        };
+    };
     //--espera todas as threads terminarem
     for (t = 0; t < nthreads; t++)
     {
@@ -135,25 +144,17 @@ int main(int argc, char *argv[])
         }
     }
     pthread_mutex_destroy(&mutex);
-    GET_TIME(end);
 
+    GET_TIME(end);
     concurrentTime = end - start;
     printf("Tempo concorrente = %lf\n", concurrentTime);
 
-    for (int i = 0; i < N; i++)
-    {
-        if (concurrentArray[i] != sequentialArray[i])
-        {
-            printf("ERRO: Matrizes diferentes.\n");
-            break;
-        }
-        else
-            printf("Matrizes iguais.\n");
-        break;
-    }
+    printf("Ganho de desempenho =  %lf\n", sequentialTime / concurrentTime);
 
-    printf("Ganho de aceleração: %lf\n", sequentialTime / concurrentTime);
+    // corretude
+    correctness(concurrentArray, sequentialArray);
 
+    free(array);
     free(sequentialArray);
     free(concurrentArray);
     free(tid);
