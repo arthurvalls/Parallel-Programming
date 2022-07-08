1b5b770e936cb69ac2f9a32aaba91a67e51499e9
Arthur Valls <73508310+arthurvalls@users.noreply.github.com>
2022-07-08 18:34:05 -0300

    Update lab10.c

--- Lab 10/lab10.c
+++ Lab 10/lab10.c
@@ -8,8 +8,8 @@
 #include <unistd.h>
 #include <semaphore.h>
 
-#define L 2 // numero de threads leitoras
-#define E 4 // numero de threads escritoras
+#define L 3 // numero de threads leitoras
+#define E 3 // numero de threads escritoras
 
 #define NTHREADS L + E
 
@@ -22,7 +22,7 @@ void *Leitor(void *threadid)
    int *tid = (int *)threadid;
    while (1)
    {
-      printf("Leitora %d quer ler!\n", *tid);
+      printf("Leitor %d quer ler!\n", *tid);
       sem_wait(&leit);
       sem_wait(&em_l);
       l++;
@@ -32,9 +32,9 @@ void *Leitor(void *threadid)
       }
       sem_post(&em_l);
       sem_post(&leit);
-      printf("Leitora %d está lendo!\n", *tid);
+      printf("Leitor %d está lendo!\n", *tid);
       sleep(1);
-      printf("Leitora %d terminou de ler!\n", *tid);
+      printf("Leitor %d terminou de ler!\n", *tid);
       sem_wait(&em_l);
       l--;
       if (l == 0)
@@ -53,7 +53,7 @@ void *Escritor(void *threadid)
    int *tid = (int *)threadid;
    while (1)
    {
-      printf("Escritora %d quer escrever!\n", *tid);
+      printf("Escritor %d quer escrever!\n", *tid);
       sem_wait(&em_e);
       e++;
       if (e == 1)
@@ -62,9 +62,9 @@ void *Escritor(void *threadid)
       }
       sem_post(&em_e);
       sem_wait(&escr);
-      printf("Escritora %d está escrevendo!\n", *tid);
+      printf("Escritor %d está escrevendo!\n", *tid);
       sem_post(&escr);
-      printf("Escritora %d terminou de escrever!\n", *tid);
+      printf("Escritor %d terminou de escrever!\n", *tid);
       sem_wait(&em_e);
       e--;
       if (e == 0)
@@ -84,29 +84,35 @@ int main(void)
    pthread_t tid[NTHREADS];
    int t, id[NTHREADS];
 
-   //inicia os semaforos
+   // inicia os semaforos
    sem_init(&leit, 0, 1);
    sem_init(&escr, 0, 1);
    sem_init(&em_l, 0, 1);
    sem_init(&em_e, 0, 1);
 
-   // cria os leitores 
-   for (t = 0; t < L; t++)
-   {
-      id[t] = t + 1;
-      if (pthread_create(&tid[t], NULL, Leitor, (void *)&id[t]))
-         exit(-1);
-   }
-
-   // cria os escritores 
+   // cria os escritores
    for (t = 0; t < E; t++)
    {
       id[t + L] = t + 1;
       if (pthread_create(&tid[t + L], NULL, Escritor, (void *)&id[t + L]))
+      {
+         printf("--ERRO: pthread_create()\n");
          exit(-1);
+      }
+   }
+   // cria os leitores
+   for (t = 0; t < L; t++)
+   {
+      id[t] = t + 1;
+      if (pthread_create(&tid[t], NULL, Leitor, (void *)&id[t]))
+      {
+         printf("--ERRO: pthread_create()\n");
+         exit(-1);
+      }
    }
 
-   // aguarda as threads terminarem
+
+   // --espera todas as threads terminarem
    for (t = 0; t < (NTHREADS); t++)
    {
       pthread_join(tid[t], NULL);
